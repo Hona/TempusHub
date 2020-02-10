3e7595a6055139cb65b2fedfdea1124b03ed9aa3
Luke Parker <lukemparker@outlook.com>
2020-02-10 20:51:13 +1000

    Create automated install and run script
    Exposes port 80

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Build the image with the name 'tempus-hub'
+echo -= Building Docker Image from Dockerfile =-\n
+docker build -t tempus-hub ./src/TempusHubBlazor/
+
+echo -= Runnning the Image (bound to host port 80) =-\n
+# Bind port '80:' of host to ':80' of container
+docker run -d -p 80:80 tempus-hub
