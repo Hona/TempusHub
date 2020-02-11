e8fa22450bf772871e45259c63da774545fe5338
Luke Parker <lukemparker@outlook.com>
2020-02-11 15:45:56 +1000

    Bind port 443 of container to host

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ docker build -t tempus-hub ./src/TempusHubBlazor/
 
 echo '-= Runnning the Image (bound to host port 80) =-\n'
 # Bind port '80:' of host to ':80' of container
-docker run -d -p 80:80 tempus-hub
+docker run -d -p 80:80 -p 443:443 tempus-hub
