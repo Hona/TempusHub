e1a954ea94c6fc4fd33792b0ccc22f9caab933e9
Luke Parker <lukemparker@outlook.com>
2020-02-13 21:06:40 +1000

    Return one level of timespan accuracy

--- src/TempusHubBlazor/Utilities/DateTimeExtensions.cs
+++ src/TempusHubBlazor/Utilities/DateTimeExtensions.cs
@@ -20,11 +20,11 @@ namespace TempusHubBlazor.Utilities
 
             var sb = new StringBuilder();
             if (span.Days > 0)
-                sb.AppendFormat("{0} day{1} ", span.Days, span.Days > 1 ? "s" : String.Empty);
+                return sb.AppendFormat("{0} day{1} ", span.Days, span.Days > 1 ? "s" : string.Empty).ToString();
             if (span.Hours > 0)
-                sb.AppendFormat("{0} hour{1} ", span.Hours, span.Hours > 1 ? "s" : String.Empty);
+                return sb.AppendFormat("{0} hour{1} ", span.Hours, span.Hours > 1 ? "s" : string.Empty).ToString();
             if (span.Minutes > 0)
-                sb.AppendFormat("{0} minute{1} ", span.Minutes, span.Minutes > 1 ? "s" : String.Empty);
+                return sb.AppendFormat("{0} minute{1} ", span.Minutes, span.Minutes > 1 ? "s" : string.Empty).ToString();
             return sb.ToString();
 
         }
