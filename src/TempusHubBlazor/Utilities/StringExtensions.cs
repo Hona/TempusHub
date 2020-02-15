55c81f47b8a58c53253364c513facafbaefc6a52
Luke Parker <lukemparker@outlook.com>
2020-02-15 14:03:37 +1000

    Add a function to use standard casing, and a dropdown to change between recent record type

---
+++ src/TempusHubBlazor/Utilities/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempusHubBlazor.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string ToStandardCasing(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+        }
+    }
+}
