b1420f14af4eedc45aa0e6fbfbb0cad4f62a4ee3
Luke Parker <lukemparker@outlook.com>
2020-02-17 19:37:36 +1000

    Refactor TempusCacheService into smaller functions and document it

--- src/TempusHubBlazor/Services/TempusCacheService.cs
+++ src/TempusHubBlazor/Services/TempusCacheService.cs
@@ -27,68 +27,83 @@ namespace TempusHubBlazor.Services
         
         private async Task UpdateAllCachedDataAsync()
         {
-            RecentActivity = await TempusDataService.GetRecentActivityAsync();
+            var tasks = new List<Task>
+            {
+                 UpdateRecentActivityAsync().ContinueWith((taskOutput) => UpdateRecentActivityWithZonedDataAsync()),
+                 UpdateTopOnlinePlayersAsync()
+            };
 
-            // Reset model
+            await Task.WhenAll(tasks);
+        }
+        private async Task UpdateRecentActivityAsync()
+        {
+            RecentActivity = await TempusDataService.GetRecentActivityAsync();
+        }
+        private async Task UpdateRecentActivityWithZonedDataAsync()
+        {
             var recentActivityWithZonedData = new RecentActivityWithZonedData();
 
             var tasks = new List<Task>();
-
             tasks.AddRange(RecentActivity.MapRecords.Select(async x => recentActivityWithZonedData.MapWR.Add(await TempusDataService.PopulateRecordDataAsync(x))));
             tasks.AddRange(RecentActivity.CourseRecords.Select(async x => recentActivityWithZonedData.CourseWR.Add(await TempusDataService.PopulateRecordDataAsync(x))));
             tasks.AddRange(RecentActivity.BonusRecords.Select(async x => recentActivityWithZonedData.BonusWR.Add(await TempusDataService.PopulateRecordDataAsync(x))));
             tasks.AddRange(RecentActivity.MapTopTimes.Select(async x => recentActivityWithZonedData.MapTT.Add(await TempusDataService.PopulateRecordDataAsync(x))));
-
-            tasks.Add(Task.Run(async () => 
-            {
-                var tempTopPlayersOnline = new List<TopPlayerOnline>();
-                var servers = (await TempusDataService.GetServerStatusAsync()).Where(x => x != null).ToArray();
-                var users = servers.Where(x => x.GameInfo != null &&
-                                               (x.GameInfo != null || x.ServerInfo != null ||
-                                                x.GameInfo.Users != null) &&
-                                               x.GameInfo.Users.Count != 0)
-                    .SelectMany(x => x.GameInfo.Users).Where(x => x?.Id != null).ToArray();
-
-                var userIdStrings = (from user in users where user?.Id != null select user.Id.ToString()).ToList();
-
-                var rankTasks = new List<Task<Rank>>();
-                rankTasks.AddRange(userIdStrings.Select(TempusDataService.GetUserRankAsync));
-
-
-                var ranks = await Task.WhenAll(rankTasks);
-                var rankedUsers = ranks.ToDictionary(rank => users.First(x => x.Id == rank.PlayerInfo.Id), rank =>
-                    rank.ClassRankInfo.DemoRank.Rank <= rank.ClassRankInfo.SoldierRank.Rank
-                        ? rank.ClassRankInfo.DemoRank.Rank
-                        : rank.ClassRankInfo.SoldierRank.Rank);
-
-                var output = rankedUsers.OrderBy(x => x.Value);
-
-
-                foreach (var (key, value) in output)
-                {
-                    if (key == null || value > 200) continue;
-                    var server = servers
-                        .FirstOrDefault(x =>
-                            x.GameInfo?.Users != null &&
-                            x.GameInfo.Users.Count(z => z.Id.HasValue && z.Id == key.Id) != 0);
-                    if (server == null || key.Id == null) continue;
-                    tempTopPlayersOnline.Add(new TopPlayerOnline 
-                    { 
-                        Rank = value,
-                        SteamId = key.SteamId,
-                        Name = key.Name,
-                        Server = server
-                    });
-                }
-
-                TopPlayersOnline = tempTopPlayersOnline;
-
-                return Task.CompletedTask;
-            }));
-
             await Task.WhenAll(tasks);
 
             RecentActivityWithZonedData = recentActivityWithZonedData;
+        }
+        private async Task UpdateTopOnlinePlayersAsync()
+        {
+            var tempTopPlayersOnline = new List<TopPlayerOnline>();
+
+            // Get online servers
+            var servers = (await TempusDataService.GetServerStatusAsync()).Where(x => x != null).ToArray();
+
+            // Get all valid online users
+            var users = servers.Where(x => x.GameInfo != null &&
+                                           (x.GameInfo != null || x.ServerInfo != null ||
+                                            x.GameInfo.Users != null) &&
+                                           x.GameInfo.Users.Count != 0)
+                .SelectMany(x => x.GameInfo.Users).Where(x => x?.Id != null).ToArray();
+
+            // Get the user IDs as strings
+            var userIdStrings = (users.Where(user => user?.Id != null).Select(user => user.Id.ToString())).ToList();
+
+            // Query all at once for all users ranks
+            var rankTasks = new List<Task<Rank>>();
+            rankTasks.AddRange(userIdStrings.Select(TempusDataService.GetUserRankAsync));
+            var ranks = await Task.WhenAll(rankTasks);
+
+            // Get the users that actually have a rank (exclude unranks), and select the higher rank
+            // Dictionary<User, BestRank>
+            var rankedUsers = ranks.ToDictionary(rank => users.First(x => x.Id == rank.PlayerInfo.Id), rank =>
+                rank.ClassRankInfo.DemoRank.Rank <= rank.ClassRankInfo.SoldierRank.Rank
+                    ? rank.ClassRankInfo.DemoRank.Rank
+                    : rank.ClassRankInfo.SoldierRank.Rank);
+
+            // Sort by the best rank
+            var output = rankedUsers.OrderBy(x => x.Value);
+
+
+            foreach (var (player, rank) in output)
+            {
+                if (player == null || rank > 200) continue;
+                var server = servers
+                    .FirstOrDefault(x =>
+                        x.GameInfo?.Users != null &&
+                        x.GameInfo.Users.Count(z => z.Id.HasValue && z.Id == player.Id) != 0);
+                if (server == null || player.Id == null) continue;
+
+                tempTopPlayersOnline.Add(new TopPlayerOnline
+                {
+                    Rank = rank,
+                    SteamId = player.SteamId,
+                    Name = player.Name,
+                    Server = server
+                });
+            }
+
+            TopPlayersOnline = tempTopPlayersOnline;
         }
     }
 }
