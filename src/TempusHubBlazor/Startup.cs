3dd6772c9890b45c1f0b8083c31a11f595b2c1f8
Luke Parker <lukemparker@outlook.com>
2020-02-17 18:11:28 +1000

    Add using statement for services

--- src/TempusHubBlazor/Startup.cs
+++ src/TempusHubBlazor/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TempusHubBlazor.Data;
+using TempusHubBlazor.Services;
 
 namespace TempusHubBlazor
 {
