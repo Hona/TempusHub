849f8737a058eabe8e631cf1f2c2b54f3f404a19
Luke Parker <lukemparker@outlook.com>
2020-02-17 16:20:27 +1000

    Add soldier and demo icons instead of 'S' and 'D'

---
+++ src/TempusHubBlazor/Constants/LocalFileConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempusHubBlazor.Constants
+{
+    public static class LocalFileConstants
+    {
+        public static readonly string DemomanIcon = "/img/demoman.png";
+        public static readonly string SoldierIcon = "/img/soldier.png";
+    }
+}
