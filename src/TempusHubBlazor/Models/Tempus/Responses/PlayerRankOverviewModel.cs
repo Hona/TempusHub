bc3f6991c3be794af3b85c143ac5291fe0234743
Luke Parker <lukemparker@outlook.com>
2020-02-20 16:51:09 +1000

    Add player rank overview model

---
+++ src/TempusHubBlazor/Models/Tempus/Responses/PlayerRankOverviewModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempusHubBlazor.Models.Tempus.Responses
+{
+    public class PlayerRankOverviewModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "steamid")]
+        public int SteamId { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public int Name { get; set; }
+        [JsonProperty(PropertyName = "points")]
+        public double Points { get; set; }
+        [JsonProperty(PropertyName = "rank")]
+        public int Rank { get; set; }
+    }
+}
---
+++ src/TempusHubBlazor/Models/Tempus/Responses/RanksOverviewModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempusHubBlazor.Models.Tempus.Responses
+{
+    public class RanksOverviewModel
+    {
+        [JsonProperty(PropertyName = "count")]
+        public int PlayerCount { get; set; }
+        [JsonProperty(PropertyName = "players")]
+        public List<PlayerRankOverviewModel> TopPlayers { get; set; }
+    }
+}
