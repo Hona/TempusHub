f901b7c5233aff33ecad5fa404445e5ddb55fd77
Luke Parker <lukemparker@outlook.com>
2020-02-20 17:26:23 +1000

    Create HrefHelper.cs

---
+++ src/TempusHubBlazor/Utilities/HrefHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempusHubBlazor.Utilities
+{
+    public static class HrefHelper
+    {
+        public static string GetMapInfoPath(string mapName) => "/maps/" + mapName;
+    }
+}
