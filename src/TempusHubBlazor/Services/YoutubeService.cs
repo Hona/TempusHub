fe573adc27730f159618934a865b94643733db21
Luke Parker <lukemparker@outlook.com>
2020-02-21 13:42:21 +1000

    Add TF2RJweekly video componet to index

---
+++ src/TempusHubBlazor/Services/YoutubeService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace TempusHubBlazor.Services
+{
+    public class YoutubeAPIService
+    {
+        private YouTubeService _youtubeService;
+        public YoutubeAPIService()
+        {
+            var baseGoogleService = new BaseClientService.Initializer()
+            {
+                ApiKey = Environment.GetEnvironmentVariable("YOUTUBE_API_KEY")
+            };
+            _youtubeService = new YouTubeService(baseGoogleService);
+        }
+        public async Task<List<PlaylistItem>> GetAllVideosAsync(string channelParam, bool isId = false)
+        {
+            var output = new List<PlaylistItem>();
+
+            var channelRequest = _youtubeService.Channels.List("contentDetails");
+
+            // Specify the channel
+            if (isId)
+            {
+                channelRequest.Id = channelParam;
+            }
+            else
+            {
+                channelRequest.ForUsername = channelParam;
+            }
+
+            // Get the response
+            var channelResponse = await channelRequest.ExecuteAsync();
+            var channel = channelResponse.Items.First();
+            var uploadPlaylistId = channel.ContentDetails.RelatedPlaylists.Uploads;
+
+            var nextUploadPageToken = "";
+            while (nextUploadPageToken != null)
+            {
+                var uploadListRequest = _youtubeService.PlaylistItems.List("snippet");
+                uploadListRequest.PlaylistId = uploadPlaylistId;
+                uploadListRequest.MaxResults = 50;
+                uploadListRequest.PageToken = nextUploadPageToken;
+
+                var uploadListResponse = await uploadListRequest.ExecuteAsync();
+                var uploads = uploadListResponse.Items;
+
+                output.AddRange(uploads);
+
+                // Get the next page
+                nextUploadPageToken = uploadListResponse.NextPageToken;
+            }
+
+            return output;
+        }
+    }
+}
--- src/TempusHubBlazor/Startup.cs
+++ src/TempusHubBlazor/Startup.cs
@@ -38,6 +38,7 @@ namespace TempusHubBlazor
             }
             services.AddSingleton(tempusDataService);
             services.AddSingleton(new TempusCacheService(tempusDataService));
+            services.AddSingleton<YoutubeAPIService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
