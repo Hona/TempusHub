a689828f6bdc1845550534f4c058d3d078258a5a
TheRealHona <lukemparker@outlook.com>
2020-03-02 13:08:11 +1000

    Don't use https redirection

--- src/TempusHubBlazor/Startup.cs
+++ src/TempusHubBlazor/Startup.cs
@@ -55,7 +55,7 @@ namespace TempusHubBlazor
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
