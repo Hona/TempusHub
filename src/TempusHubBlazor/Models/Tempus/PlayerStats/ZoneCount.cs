1b17f7b602cd2f8fdc85a35e3c53fe23b8d146c8
Luke Parker <10430890+TheRealHona@users.noreply.github.com>
2020-03-02 14:08:41 +1000

    Add player WR/Top Time stats to Player page, and fix page loading (#28)
    * Don't use Hsts and get https port from env var
    
    * Add GitHub and Discord links to the top
    
    * Revert "Don't use Hsts and get https port from env var"
    
    This reverts commit bff0d3acd3f55722e3868dc3a97843185b7d27be.
    
    * Use newlines
    
    * Wrap heading links in <p>
    
    * Don't use https redirection
    
    * Use span instead of <p>
    
    * Add scripts to body
    
    * Add player stats and data service entry
    
    * Add WR/Top Time entries to player page
    
    * Don't use lists, instead <p> and add 'points' after points count

---
+++ src/TempusHubBlazor/Models/Tempus/PlayerStats/ZoneCount.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempusHubBlazor.Models.Tempus.PlayerStats
+{
+    public class ZoneCount
+    {
+        [JsonProperty(PropertyName = "count")]
+        public long Count { get; set; }
+    }
+}
