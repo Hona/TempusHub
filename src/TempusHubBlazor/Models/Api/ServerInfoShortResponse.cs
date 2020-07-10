fecae92bed31122623158ef019cf792ae56f76c3
Luke Parker <lukemparker@outlook.com>
2020-07-10 12:42:28 +1000

    Create top player online API

---
+++ src/TempusHubBlazor/Models/Api/ServerInfoShortResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace TempusHubBlazor.Models.Api
+{
+    public class ServerInfoShortResponse
+    {
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        public int CurrentPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public string CurrentMap { get; set; }
+        public string IpAddress { get; set; }
+        public int Id { get; set; }
+    }
+}
