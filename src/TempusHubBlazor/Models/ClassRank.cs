4b40139f9f47a2139507d665896b19ed9dfe9329
Luke Parker <lukemparker@outlook.com>
2020-07-10 12:40:46 +1000

    Create ClassRank.cs

---
+++ src/TempusHubBlazor/Models/ClassRank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TempusHubBlazor.Models.Tempus;
+
+namespace TempusHubBlazor.Models
+{
+    public class ClassRank
+    {
+        public int Class { get; set; }
+        public int Rank { get; set; }
+        public ServerPlayerModel Player { get; set; }
+    }
+}
