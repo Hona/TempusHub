fe9dd4df7088a0f1b650e7730f45674acfd36756
Luke Parker <lukemparker@outlook.com>
2020-07-15 21:55:51 +1000

    Add Map/Course/Bonus WR + Map TT API endpoints

--- src/TempusHubBlazor/Api/ActivityController.cs
+++ src/TempusHubBlazor/Api/ActivityController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 using TempusHubBlazor.Models;
+using TempusHubBlazor.Models.Api;
+using TempusHubBlazor.Models.Tempus.Activity;
 using TempusHubBlazor.Models.Tempus.Responses;
 using TempusHubBlazor.Services;
 
@@ -20,15 +24,85 @@ namespace TempusHubBlazor.Api
             _tempusCacheService = tempusCacheService;
         }
 
-        [HttpGet]
-        public ActionResult<RecentActivityWithZonedData> Get()
+        private List<RecordResponse> TransformRecordData(List<RecordWithZonedData> recordBase)
+        {
+            var output = recordBase.Select(x => new RecordResponse
+            {
+                PlayerInfo = new PlayerResponse
+                {
+                    Id = x.Record.PlayerInfo.Id,
+                    Steamid = x.Record.PlayerInfo.Steamid,
+                    Name = x.Record.PlayerInfo.Name,
+                    RealName = null
+                },
+                CachedTime = x.Record.CachedTime,
+                MapInfo = x.Record.MapInfo,
+                RecordInfo = x.Record.RecordInfo,
+                ZoneInfo = x.Record.ZoneInfo
+            }).ToList();
+
+            foreach (var record in output)
+            {
+                var realName = _tempusCacheService.GetRealName(record.PlayerInfo.Id);
+                if (realName != null)
+                {
+                    var realNamePlayer = new PlayerResponse
+                    {
+                        Id = record.PlayerInfo.Id,
+                        Name = record.PlayerInfo.Name,
+                        Steamid = record.PlayerInfo.Steamid,
+                        RealName = realName
+                    };
+
+                    record.PlayerInfo = realNamePlayer;
+                }
+            }
+
+            return output;
+        }
+
+        [HttpGet("MapWR")]
+        public ActionResult<List<RecordResponse>> GetMapWR()
         {
             if (_tempusCacheService.RecentActivityWithZonedData == null)
             {
                 return NoContent();
             }
 
-            return _tempusCacheService.RecentActivityWithZonedData;
+            return TransformRecordData(_tempusCacheService.RecentActivityWithZonedData.MapWR);
+        }
+
+        [HttpGet("CourseWR")]
+        public ActionResult<List<RecordResponse>> GetCourseWR()
+        {
+            if (_tempusCacheService.RecentActivityWithZonedData == null)
+            {
+                return NoContent();
+            }
+
+            return TransformRecordData(_tempusCacheService.RecentActivityWithZonedData.CourseWR);
+        }
+
+        [HttpGet("BonusWR")]
+        public ActionResult<List<RecordResponse>> GetBonusWR()
+        {
+            if (_tempusCacheService.RecentActivityWithZonedData == null)
+            {
+                return NoContent();
+            }
+
+            return TransformRecordData(_tempusCacheService.RecentActivityWithZonedData.BonusWR);
+        }
+
+        [HttpGet("MapTT")]
+        public ActionResult<List<RecordResponse>> GetMapTT()
+        {
+            if (_tempusCacheService.RecentActivityWithZonedData == null)
+            {
+                return NoContent();
+            }
+
+            return TransformRecordData(_tempusCacheService.RecentActivityWithZonedData.MapTT);
         }
     }
 }
