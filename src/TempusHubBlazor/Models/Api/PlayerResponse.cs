204454ae05ceef70094d8d26475cc39ebfef8761
Luke Parker <lukemparker@outlook.com>
2020-07-15 21:55:23 +1000

    Create PlayerResponse.cs

---
+++ src/TempusHubBlazor/Models/Api/PlayerResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TempusHubBlazor.Models.Tempus.Activity;
+
+namespace TempusHubBlazor.Models.Api
+{
+    public class PlayerResponse : PlayerInfo
+    {
+        public string RealName { get; set; }
+    }
+}
