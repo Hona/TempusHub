0485291ce1d751e4936c1cbf9d4cbb4645a506d0
Luke Parker <lukemparker@outlook.com>
2020-07-15 21:55:26 +1000

    Create RecordResponse.cs

---
+++ src/TempusHubBlazor/Models/Api/RecordResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TempusHubBlazor.Models.MySQL;
+using TempusHubBlazor.Models.Tempus.Activity;
+
+namespace TempusHubBlazor.Models.Api
+{
+    public class RecordResponse
+    {
+        public MapRecordCache CachedTime { get; set; }
+
+        public RecordInfoShort RecordInfo { get; set; }
+
+        public ZoneInfo ZoneInfo { get; set; }
+
+        public MapInfo MapInfo { get; set; }
+
+        public PlayerResponse PlayerInfo { get; set; }
+    }
+}
