4a95200ef245b4ddb0200652b02caec9aac7b915
Luke Parker <lukemparker@outlook.com>
2020-08-01 11:12:02 +1000

    Remove HSTS from production (forwarded headers)

--- src/TempusHubBlazor/Startup.cs
+++ src/TempusHubBlazor/Startup.cs
@@ -56,7 +56,6 @@ namespace TempusHubBlazor
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
