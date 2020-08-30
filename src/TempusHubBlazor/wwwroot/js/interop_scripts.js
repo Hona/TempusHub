3bd1e79efc0ab61dc660ec2cd8915fb5c479d5f7
Luke Parker <lukemparker@outlook.com>
2020-08-30 13:30:56 +1000

    Add interop script, and remove duplicate script

---
+++ src/TempusHubBlazor/wwwroot/js/interop_scripts.js
@@ -0,0 +1,9 @@
+﻿window.searchFunctions = {
+    clickSearch: function (id) {
+        const searchButton = document.getElementById(id);
+        // Next sibling is the dropdown
+        if (!searchButton.nextElementSibling.classList.contains("show")) {
+            searchButton.click();
+        }
+    }
+};
